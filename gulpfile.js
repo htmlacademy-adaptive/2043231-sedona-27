221a244420307397ddc50b0052c35d4c4933947e
ElenaAndryushihina <karetina1991@gmail.com>
2023-01-11 19:18:53 +0300

    Доработка автоматизации

--- gulpfile.js
+++ gulpfile.js
@@ -71,9 +71,9 @@ const createWebp = () => {
 // SVG
 
 const svg = () =>
-  gulp.src(['source/img/svg/*.svg', '!source/img/svg/icons/*.svg'])
+  gulp.src(['source/img/**/*.svg'])
     .pipe(svgo())
-    .pipe(gulp.dest('build/img/svg'));
+    .pipe(gulp.dest('build/img'));
 
 const sprite = () => {
   return gulp.src('source/img/svg/icons/*.svg')
@@ -91,6 +91,7 @@ const copy = (done) => {
   gulp.src([
     'source/fonts/*.{woff2,woff}',
     'source/*.ico',
+    'source/manifest.webmanifest',
   ], {
   base: 'source'
 })
